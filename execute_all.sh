e824097914e895ffc2f34cc3017be4975eb9ff44
Orlando Jr. Rengifo <107599003+OrlandoJrRengifo@users.noreply.github.com>
2025-02-14 09:10:31 -0500

    asd

--- execute_all.sh
+++ execute_all.sh
@@ -11,17 +11,19 @@ for dir in */; do
   if [ -d "$dir" ]; then
     LENGUAJE=$(basename "$dir")
     echo "🔹 Ejecutando $LENGUAJE..."
-
-    # Construir imagen Docker (montando el socket local)
+    
+    # Construir imagen Docker
     docker build -t "${LENGUAJE//+/}-benchmark" "$dir"
-
-    # Ejecutar contenedor y capturar tiempo (montando el socket local)
+    
+    # Ejecutar contenedor y capturar tiempo
     TIEMPO=$(docker run --rm -v /var/run/docker.sock:/var/run/docker.sock "${LENGUAJE//+/}-benchmark")
-
+    
     if [ -n "$TIEMPO" ]; then
-      echo "$LENGUAJE: $TIEMPO ms"
+      echo "✅ $LENGUAJE: $TIEMPO ms"
     else
       echo "❌ Error: Salida inesperada del contenedor para $LENGUAJE"
     fi
   fi
 done
+
+echo "✅ Benchmarks completados."
